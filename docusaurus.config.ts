88c3442a8bd3886d91c313c5d0cb54b3b032354c
Rogersovich <53912205+rogersovich@users.noreply.github.com>
2026-05-27 08:47:14 +0700

    update favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
       title: "Kalend",
       logo: {
         alt: "Kalend Logo",
-        src: "img/logo.svg",
+        src: "img/logo.png",
       },
       items: [
         {
